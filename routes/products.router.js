5063347e15b115b0fec38064b234ee9537b8f411
Cristian Franco <cfrancobedoya@gmail.com>
2023-06-10 15:34:38 -0500

    MAIN: Add service to manage the clean architecture in the API

--- routes/products.router.js
+++ routes/products.router.js
@@ -1,21 +1,12 @@
 const express = require('express');
-const { faker } = require('@faker-js/faker');
+
+const ProductsService = require('../services/products.service');
 
 const router = express.Router();
+const service = new ProductsService();
 
 router.get('/', (req, res) => {
-  const products = [];
-  const { size } = req.query;
-
-  const limit = size || 10;
-
-  for (let index = 0; index < limit; index++) {
-    products.push({
-      name: faker.commerce.productName(),
-      price: parseInt(faker.commerce.price(), 10),
-      image: faker.image.url(),
-    });
-  }
+  const products = service.find();
   res.json(products);
 });
 
@@ -25,18 +16,8 @@ router.get('/filter', (req, res) => {
 
 router.get('/:id', (req, res) => {
   const { id } = req.params;
-
-  if (id === '999') {
-    res.status(404).json({
-      message: 'Not found',
-    });
-  } else {
-    res.json({
-      id: id,
-      name: 'Product 1',
-      price: 100,
-    });
-  }
+  const product = service.findOne(id);
+  res.json(product);
 });
 
 router.post('/', (req, res) => {
