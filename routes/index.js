1dda4fa84db2518a9b4b779f15e3f1e10c51c22c
Cristian Franco <cfrancobedoya@gmail.com>
2023-06-09 22:53:10 -0500

    MAIN: Add post method to Products router

--- routes/index.js
+++ routes/index.js
@@ -1,11 +1,15 @@
+const express = require('express');
 const productsRouter = require('./products.router');
 const usersRouter = require('./users.router');
 const categoriesRouter = require('./categories.router');
 
 function routerApi(app) {
-  app.use('/categories', categoriesRouter);
-  app.use('/products', productsRouter);
-  app.use('/users', usersRouter);
+  const router = express.Router();
+  app.use('/api/v1', router);
+
+  router.use('/categories', categoriesRouter);
+  router.use('/products', productsRouter);
+  router.use('/users', usersRouter);
 }
 
 module.exports = routerApi;
